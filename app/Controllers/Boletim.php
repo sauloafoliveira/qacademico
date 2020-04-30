a3b681ed17ca8f9680e6112b817918651b275c87
Caleb Almeida <calebfelixep@gmail.com>
2020-04-30 19:05:07 -0300

    subindo arquivos essenciais do codeigniter e finbalizando a atividade 1 (criação dos controllers e views boletin e frequencia)

---
+++ app/Controllers/Boletim.php
@@ -0,0 +1,7 @@
+<?php namespace App\Controllers;
+
+class Boletim extends BaseController{
+    public function index(){
+        return view('boletim');
+    }
+}
---
+++ app/Controllers/Frequencia.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class Frequencia extends BaseController{
+
+    public function index(){
+        return view("frequencia");
+    }
+}
+
