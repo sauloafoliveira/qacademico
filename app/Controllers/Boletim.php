b8d81189f988ff64257a67dea30ec9c4460fc568
Caleb Almeida <calebfelixep@gmail.com>
2020-05-23 18:10:59 -0300

    deletando os arquivos para mudar a versão do codeIgniter

--- app/Controllers/Boletim.php
+++
@@ -1,7 +0,0 @@
-<?php namespace App\Controllers;
-
-class Boletim extends BaseController{
-    public function index(){
-        return view('boletim');
-    }
-}
--- app/Controllers/Frequencia.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Controllers;
-
-class Frequencia extends BaseController{
-
-    public function index(){
-        return view("frequencia");
-    }
-}
-
